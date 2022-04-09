176fb3985070f1ab7f83fb611563943463985d1b
Alcides Oliveira Alcoforado <alcidesce@gmail.com>
2022-04-09 07:47:47 -0300

    Implementando o prepare no PDO para segurança de SQL injection

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -6,10 +6,23 @@ require_once 'vendor/autoload.php';
 
 require 'conexao.php';
 
-$estudante = new Student(null, 'Alcides Alcoforado', new \DateTimeImmutable('1980-07-26'));
+$estudante = new Student(
+    null, 
+    'Rafaela Alcoforado', 
+    new \DateTimeImmutable('2014-04-14')
+    );
 
-$sqlInsert = "INSERT INTO STUDENTS (name,birth_date) VALUES ('{$estudante->name()}','{$estudante->birthDate()->format('Y-m-d')}');";
+//$sqlInsert = "INSERT INTO STUDENTS (name,birth_date) VALUES ('{$estudante->name()}','{$estudante->birthDate()->format('Y-m-d')}');";
+
+$sqlInsert = "INSERT INTO STUDENTS (name,birth_date) VALUES (:name,:birth_date);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $estudante->name());
+$statement->bindValue(':birth_date', $estudante->birthDate()->format('Y-m-d'));
+
+if ($statement->execute()){
+    echo "Aluno incluido";
+}
 
 //$sqlInsert = "DELETE FROM ESTUDANTE ";
 
-var_dump($pdo->exec($sqlInsert));
+//var_dump($pdo->exec($sqlInsert));
